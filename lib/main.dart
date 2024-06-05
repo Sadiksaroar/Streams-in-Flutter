57600e73dc0113058495f88a4a01b38bf675657c
Sadik Saroar <91769816+Sadiksaroar@users.noreply.github.com>
2024-06-05 13:33:04 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Stream Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  final StreamController<int> _streamController = StreamController<int>();
+  int _counter = 0;
+
+  void _incrementCounter() {
+    _counter++;
+    _streamController.sink.add(_counter);
+  }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Stream Demo'),
+      ),
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: _streamController.stream,
+          initialData: _counter,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Text(
+                'Counter: ${snapshot.data}',
+                style: TextStyle(fontSize: 24),
+              );
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
